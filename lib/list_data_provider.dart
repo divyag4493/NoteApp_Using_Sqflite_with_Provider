b62142c73328baaa5ff903f8ab2c6543fb3cbeb4
DELL <dcgajera20@gmail.com>
2024-01-27 18:50:45 +0530

    - list data

--- lib/list_data_provider.dart
+++ lib/list_data_provider.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/foundation.dart';
 
-class ListDataProvider extends ChangeNotifier{
-  List<Map<String,dynamic>> _myList = [];
+class ListDataProvider extends ChangeNotifier {
+  List<Map<String, dynamic>> _myList = [];
 
-  List<Map<String,dynamic>> getMyList(){
+  List<Map<String, dynamic>> getMyList() {
     return _myList;
   }
 
   /// action[Events]
 
-addData(Map<String,dynamic> newData){
+  addData(Map<String, dynamic> newData) {
     _myList.add(newData);
     notifyListeners();
-}
+  }
 
-updateData(Map<String,dynamic> dataToBeUpdated, int index){
+  updateData(Map<String, dynamic> dataToBeUpdated, int index) {
     _myList[index] = dataToBeUpdated;
     notifyListeners();
-}
+  }
 
-deleteData(int index){
+  deleteData(int index) {
     _myList.removeAt(index);
     notifyListeners();
-}
-
+  }
 }
